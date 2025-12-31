408358201c908d547a66c5999b826c61f74a9006
YO_DI3G0 <diegocda1212@gmail.com>
2025-12-30 22:15:38 -0600

    Update base64.lua

--- base64.lua
+++ base64.lua
@@ -15,17 +15,18 @@ function module.encode(data)
 		local a = data:byte(i) or 0
 		local b = data:byte(i+1) or 0
 		local c = data:byte(i+2) or 0
-		local n = a << 16 | b << 8 | c
-		result[#result+1] = chars:sub((n >> 18 & 63)+1,(n >> 18 & 63)+1)
-		result[#result+1] = chars:sub((n >> 12 & 63)+1,(n >> 12 & 63)+1)
-		result[#result+1] = chars:sub((n >> 6 & 63)+1,(n >> 6 & 63)+1)
-		result[#result+1] = chars:sub((n & 63)+1,(n & 63)+1)
+		local n = a*65536 + b*256 + c
+		result[#result+1] = chars:sub(math.floor(n/262144)%64+1, math.floor(n/262144)%64+1)
+		result[#result+1] = chars:sub(math.floor(n/4096)%64+1, math.floor(n/4096)%64+1)
+		result[#result+1] = chars:sub(math.floor(n/64)%64+1, math.floor(n/64)%64+1)
+		result[#result+1] = chars:sub(n%64+1, n%64+1)
 		i = i + 3
 	end
-	if #data % 3 == 1 then
+	local rem = #data % 3
+	if rem == 1 then
 		result[#result] = padding
 		result[#result-1] = padding
-	elseif #data % 3 == 2 then
+	elseif rem == 2 then
 		result[#result] = padding
 	end
 	return table.concat(result)
@@ -39,10 +40,10 @@ function module.decode(data)
 		local b = charToIndex[data:sub(i+1,i+1)] or 0
 		local c = charToIndex[data:sub(i+2,i+2)] or 0
 		local d = charToIndex[data:sub(i+3,i+3)] or 0
-		local n = a << 18 | b << 12 | c << 6 | d
-		result[#result+1] = string.char((n >> 16) & 255)
-		result[#result+1] = string.char((n >> 8) & 255)
-		result[#result+1] = string.char(n & 255)
+		local n = a*262144 + b*4096 + c*64 + d
+		result[#result+1] = string.char(math.floor(n/65536)%256)
+		result[#result+1] = string.char(math.floor(n/256)%256)
+		result[#result+1] = string.char(n%256)
 		i = i + 4
 	end
 	local pad = 0
